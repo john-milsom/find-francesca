761371c90a505b560eaf9035fde84be26b59c35f
milsomj <milsomj@tcd.ie>
2025-05-24 17:58:07 +0100

    firebase authentication

--- apps/frontend/src/firebase.js
+++ apps/frontend/src/firebase.js
@@ -2,10 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  // ...other config values from your Firebase project
+  apiKey: "AIzaSyCG0Y4EAPsaSC-jTIi2g7MoAqn6PnMkZ-0",
+  authDomain: "find-francesca-auth.firebaseapp.com",
+  projectId: "find-francesca-auth",
+  storageBucket: "find-francesca-auth.firebasestorage.app",
+  messagingSenderId: "209376793518",
+  appId: "1:209376793518:web:d85d81920a0936b8a6ae3f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
